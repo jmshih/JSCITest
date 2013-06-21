ac3cfcdffac2421efd55b8ca946e18d492f1877d
Jonathan Shih <jonathan.shih@originate.com>
2013-06-21 15:23:46 -0700

    made some changes to try to get code coverage

--- git-push-wh.sh
+++ git-push-wh.sh
@@ -6,7 +6,7 @@ BRANCH="$(git rev-parse --symbolic --abbrev-ref $(git symbolic-ref HEAD))"
 PRE_PUSH="$GIT_DIR_/hooks/pre-push"
 POST_PUSH="$GIT_DIR_/hooks/post-push"
 
-git push origin master"$@"
+git push origin "$BRANCH"
 
 test $? -eq 0 && test -x "$POST_PUSH" &&
-      exec "$POST_PUSH" "$BRANCH" "$@"
+      exec "$POST_PUSH"
